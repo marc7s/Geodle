f3633ffb5afd845d9963d451e83a8f700af60856
marc7s <marcus.schagerberg@gmail.com>
2024-05-06 17:06:40 +0200

    Temporary fix for errors in development caused by Next.js bug

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,9 @@ const commitHash = execSync('git rev-parse --short HEAD').toString().trim();
 const buildDate = new Date().toLocaleString('sv-SE');
 
 const nextConfig = {
-  output: 'export',
+  // Handle the open issue in Next.js with output: 'export' by only setting it when building for production
+  // https://github.com/vercel/next.js/issues/56253#issuecomment-2077150712
+  output: process.env.NODE_ENV === 'production' ? 'export' : undefined,
   env: {
     APP_VERSION: version,
     APP_HASH: commitHash,
